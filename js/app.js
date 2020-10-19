330351fd780268568ce28712df11943d9a4cd185
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-10-19 20:33:06 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -1,32 +1,24 @@
-const navSlide = () => {
-
-const burger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li')
-
-
-
-}
-
 //Nav-Meny & Hamburger Meny
-
-/*
 
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".header-nav");
 const navLinks = document.querySelector(".header-nav .nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
+  navbar.classList.toggle("open");
+
   links.forEach((link) => {
     link.classList.toggle("open");
   });
+  
 });
 
-hamburger.addEventListener("click", () => {
-  navbar.classList.toggle("open");
-});
 
-*/
+
+
+
+
+
+
