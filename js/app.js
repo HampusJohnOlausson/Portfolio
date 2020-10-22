c7992ee21e2f17ff1de072cd412280697d800b49
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-10-22 10:21:26 +0200

    Updated hamburger meny toggle

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 //Nav-Meny & Hamburger Meny
+const navToggle = () => {
 
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".header-nav");
 const navLinks = document.querySelector(".header-nav .nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
+//Hamburger toggle
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   navbar.classList.toggle("open");
@@ -14,6 +16,20 @@ hamburger.addEventListener("click", () => {
   });
   
 });
+
+//Nav links Toggle
+navLinks.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  navbar.classList.toggle("open");
+
+  links.forEach((link) => {
+    link.classList.toggle("open");
+  });
+  
+});
+
+}
+navToggle();
 
 
 
