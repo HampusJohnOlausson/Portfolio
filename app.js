6f723c4f0f97d3871d84dd1d20e6d59dd5ceacc8
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-11-30 18:29:52 +0100

    made the popup responsive

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ const keySlide = document.getElementsByClassName('slideKey');
 /**
  * @type {HTMLDivElement}
  */
-const benganSlide = document.getElementsByClassName('slideBengan');
+//const benganSlide = document.getElementsByClassName('slideBengan');
 /**
 * @type {HTMLSpanElement}
 */
@@ -116,7 +116,7 @@ const dotFirst = document.getElementsByClassName('first');
 /**
 * @type {HTMLSpanElement}
 */
-const dotSecond = document.getElementsByClassName('second');
+//const dotSecond = document.getElementsByClassName('second');
   
   //FindKeyProject-Loop
   for(let i = 0; i < keySlide.length; i++){
@@ -124,9 +124,9 @@ const dotSecond = document.getElementsByClassName('second');
   }
 
   //BenganProject-Loop
-  for(let i = 0; i < benganSlide.length; i++){
-    benganSlide[i].style.display = 'none';
-  }
+  // for(let i = 0; i < benganSlide.length; i++){
+  //   benganSlide[i].style.display = 'none';
+  // }
 
   slideIndex++;
 
@@ -134,25 +134,25 @@ const dotSecond = document.getElementsByClassName('second');
     slideIndex = 1;
   }
 
-  if(slideIndex > benganSlide.length){
-    slideIndex = 1;
-  }
+  // if(slideIndex > benganSlide.length){
+  //   slideIndex = 1;
+  // }
 
   //Dots-Loops
 
   for(let i = 0; i < dotFirst.length; i++) {
     dotFirst[i].className = dotFirst[i].className.replace(' active', '');
   }
-  for(let i = 0; i < dotSecond.length; i++) {
-    dotSecond[i].className = dotSecond[i].className.replace(' active', '');
-  }
+  // for(let i = 0; i < dotSecond.length; i++) {
+  //   dotSecond[i].className = dotSecond[i].className.replace(' active', '');
+  // }
 
 
   keySlide[slideIndex - 1].style.display = 'block';
-  benganSlide[slideIndex - 1].style.display = 'block';
+  // benganSlide[slideIndex - 1].style.display = 'block';
 
   dotFirst[slideIndex -1].className += ' active';
-  dotSecond[slideIndex -1].className += ' active';
+  // dotSecond[slideIndex -1].className += ' active';
 
   setTimeout(slideShow , 2700);
 };
