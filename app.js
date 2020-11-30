b7a3036eebf0ff640c41b8645a232cd64e0ca581
Hampus Olausson <hampus.john.olausson@gmail.com>
2020-11-30 13:33:56 +0100

    added animation on links and exit button on modal

--- app.js
+++ app.js
@@ -162,11 +162,16 @@ const modal = () => {
 
   const popUp = document.querySelector('.readMore');
   const modalContainer = document.querySelector('.modal-container');
+  const exit = document.querySelector('.fa-times-circle');
 
   popUp.addEventListener('click', () => {
 
     modalContainer.style.transform = 'scale(1)';
+  })
 
+  exit.addEventListener('click', () => {
+
+    modalContainer.style.transform = 'scale(0)';
   })
 }
 
