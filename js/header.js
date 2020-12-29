92f97662244e4b549ad2be16613b38df521263c6
Hampus Olausson <hampus@MacBook-Pro-2.lan>
2020-12-29 17:08:25 +0100

    added some chanhges in the index pagea and made some changes in the js and the scss

---
+++ js/header.js
@@ -0,0 +1,71 @@
+/*-------------Hamburger Menu-------------*/
+/**
+ * Function for hamburger menu.
+ * Appears at 1150px and lower. 
+ * When clicked hamburger toggle to Nav-menu.
+ * When clicked on any link in the menu hamburger toggles back to original state.
+ */
+const navToggle = () => {
+
+    /**
+     * @type {HTMLDivElement}
+     */
+    const hamburger = document.querySelector(".hamburger");
+    /**
+     * @type {HTMLNavElement}
+     */
+    const navbar = document.querySelector(".header-nav");
+    /**
+     * @type {HTMLUListElement}
+     */
+    const navLinks = document.querySelector(".nav-links");
+    /**
+     * @type {HTMLLinkElement}
+     */
+    const links = document.querySelectorAll(".nav-links li");
+    
+    //Hamburger toggle
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("open");
+      navbar.classList.toggle("open");
+    
+      links.forEach((link) => {
+        link.classList.toggle("open");
+      });
+      
+    });
+    
+    //Nav-links Toggle
+    navLinks.addEventListener("click", () => {
+      navLinks.classList.toggle("open");
+      navbar.classList.toggle("open");
+    
+      links.forEach((link) => {
+        link.classList.toggle("open");
+      });
+      
+    });
+    }
+    
+    /*-----------Smooth Scroll-----------*/
+    
+    /**
+     * Function for smooth scrolling on page.
+     */
+    const smoothScroll = () => {
+    
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    
+      anchor.addEventListener('click', function(e) {
+    
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
+    
+    };
+    
+    
