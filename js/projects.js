0a0297b9ab5d28904ae7892e0679ea176fb1f47b
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-01-08 17:25:32 +0100

    added a new project page for the new project

--- js/projects.js
+++ js/projects.js
@@ -9,13 +9,14 @@ let slideIndex = 0;
 const slideShow = () => {
 
 /**
-* @type {HTMLDivElement}
-*/
-const slideTwo = document.getElementsByClassName('slideTwo');
-/**
  * @type {HTMLDivElement}
  */
 const slideOne = document.getElementsByClassName('slideOne');
+/**
+* @type {HTMLDivElement}
+*/
+const slideTwo = document.getElementsByClassName('slideTwo');
+
 /**
 * @type {HTMLSpanElement}
 */
@@ -25,25 +26,25 @@ const dotFirst = document.getElementsByClassName('first');
 */
 const dotSecond = document.getElementsByClassName('second');
   
+
+ //Project-Loop
+ for(let i = 0; i < slideOne.length; i++){
+  slideOne[i].style.display = 'none';
+ }
   //FindKeyProject-Loop
   for(let i = 0; i < slideTwo.length; i++){
     slideTwo[i].style.display = 'none';
   }
 
-  //Project-Loop
-   for(let i = 0; i < slideOne.length; i++){
-    slideOne[i].style.display = 'none';
-   }
-
   slideIndex++;
+
+  if(slideIndex > slideOne.length){
+    slideIndex = 1;
+   }
 
   if(slideIndex > slideTwo.length){
     slideIndex = 1;
   }
-
-   if(slideIndex > slideOne.length){
-    slideIndex = 1;
-   }
 
   //Dots-Loops
 
@@ -54,9 +55,8 @@ const dotSecond = document.getElementsByClassName('second');
      dotSecond[i].className = dotSecond[i].className.replace(' active', '');
    }
 
-
-  slideTwo[slideIndex - 1].style.display = 'block';
   slideOne[slideIndex - 1].style.display = 'block';
+  slideTwo[slideIndex - 1].style.display = 'block';
 
   dotFirst[slideIndex -1].className += ' active';
   dotSecond[slideIndex -1].className += ' active';
