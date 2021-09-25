21840b696ad918dbe56ddde2c54f09cada9d1a56
hampus <hampus.john.olausson@gmail.com>
2021-09-25 11:37:47 +0200

    fixed a bug in js

--- main.js
+++ main.js
@@ -1,14 +1,71 @@
-let hamburger = document.querySelector('.hamburger');
-let menu = document.querySelector('.header-nav');
-let nav = document.querySelector('.nav-links');
-let li = document.querySelector('.nav-links li');
+window.addEventListener('load', () => {
+  navToggle();
+  smoothScroll();
+});
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('li');
-    menu.classList.toggle('open');
-    nav.classList.toggle('open');
-    //li.classList.toggle('open');
+
+const navToggle = () => {
+
+  /**
+   * @type {HTMLDivElement}
+   */
+  const hamburger = document.querySelector(".hamburger");
+  /**
+   * @type {HTMLNavElement}
+   */
+  const navbar = document.querySelector(".header-nav");
+  /**
+   * @type {HTMLUListElement}
+   */
+  const navLinks = document.querySelector(".nav-links");
+  /**
+   * @type {HTMLLinkElement}
+   */
+  const links = document.querySelectorAll(".nav-links li");
+  
+  //Hamburger toggle
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    navbar.classList.toggle("open");
+  
+    links.forEach((link) => {
+      link.classList.toggle("open");
+    });
+    
   });
-
-
+  
+  //Nav-links Toggle
+  navLinks.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    navbar.classList.toggle("open");
+  
+    links.forEach((link) => {
+      link.classList.toggle("open");
+    });
+    
+  });
+  }
+  
+  /*-----------Smooth Scroll-----------*/
+  
+  /**
+   * Function for smooth scrolling on page.
+   */
+  const smoothScroll = () => {
+  
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  
+    anchor.addEventListener('click', function(e) {
+  
+      e.preventDefault();
+      
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  };
+  
+  
 
