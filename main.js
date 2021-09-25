734815ced378abcc5d869d6ae47aad966570a0d4
hampus <hampus.john.olausson@gmail.com>
2021-09-25 11:32:45 +0200

    #

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let hamburger = document.querySelector('.hamburger');
 let menu = document.querySelector('.header-nav');
 let nav = document.querySelector('.nav-links');
-let li = document.querySelector('.li');
+let li = document.querySelector('.nav-links li');
 
 hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('.nav-links');
+    hamburger.classList.toggle('li');
     menu.classList.toggle('open');
     nav.classList.toggle('open');
-    li.classList.toggle('open');
+    //li.classList.toggle('open');
   });
+
+
 
